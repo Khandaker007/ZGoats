2e1b29aa8699025965daf5b6a78dd726735e2b2d
Khandaker Nabiul <na.ninad60@gmail.com>
2021-12-22 06:04:32 +0600

    responsive done on both landing page and coming soon page

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -11,9 +11,9 @@ import {
 
 import logo from '../../assets/logo/goats-logo.png'
 
-export default function Header({onlyLogo}) {
+export default function Header({onlyLogo, px}) {
   return (
-    <Flex justifyContent={'space-between'} direction={['column','row','row','row']} gap={3}>
+    <Flex justifyContent={'space-between'} direction={['column','row','row','row']} gap={3} px={[px ? 55 : null, px ? 70 : null]}>
       <Image src={logo} alt="zgoats logo" height={['80px','50px','50px','50px']}/>
         <Flex alignItems={'center'} justify={['center','center','center',null]} gap={30} display={onlyLogo ? 'none' : 'flex'}>
             <HStack color={'white'} spacing={['15px','30px']}>
--- src/pages/landing-page.jsx
+++ src/pages/landing-page.jsx
@@ -47,263 +47,265 @@ export default function LandingPage() {
   // const [isSmallerThan900] = useMediaQuery('(min-width: 900px)')
 
   return (
-    <Box bg={"secondary"} px={[55,70]} pt={5} pb={20}>
-      <Header />
-      {/* SECTION ONE*/}
-      <Grid 
-        templateColumns={['1fr','1fr','1fr',"repeat(2, 1fr)"]}
-        gap={10}
-        py={100}>
-        <GridItem colStart={1} colEnd={2} alignSelf={"center"}>
-          <Flex flexDirection={"column"} gap={5} alignItems={"start"}>
-            <Heading color={"white"}>
-              Lorem ipsum GOATSs <br /> porta gravida at eget
-            </Heading>
-            <Text color={"textColor"}>
-              GoatedGoats is a gamified generative art project where each
-              generated trait is a separate NFT that is interchangeable between
-              the base goats.
-            </Text>
-            <Button color={"white"} bg={"primary"} size={"sm"}>
-              Explore <ArrowForwardIcon ml={1} />
-            </Button>
-          </Flex>
-        </GridItem>
-        <GridItem colStart={[1,1,1,2]} colEnd={[1,1,1,3]} justifySelf={"center"}>
-          <Image src={heroImage} alt="hero image" height={[300,378]} />
-        </GridItem>
-      </Grid>
-      {/* SECTION ONE END*/}
+    <>
+      <Box bg={"secondary"} px={[55,70]} pt={5} pb={20}>
+        <Header />
+        {/* SECTION ONE*/}
+        <Grid 
+          templateColumns={['1fr','1fr',"repeat(2, 1fr)"]}
+          gap={10}
+          py={100}>
+          <GridItem colStart={1} colEnd={2} alignSelf={"center"}>
+            <Flex flexDirection={"column"} gap={5} alignItems={"start"}>
+              <Heading color={"white"}>
+                Lorem ipsum GOATSs <br /> porta gravida at eget
+              </Heading>
+              <Text color={"textColor"}>
+                GoatedGoats is a gamified generative art project where each
+                generated trait is a separate NFT that is interchangeable between
+                the base goats.
+              </Text>
+              <Button color={"white"} bg={"primary"} size={"sm"}>
+                Explore <ArrowForwardIcon ml={1} />
+              </Button>
+            </Flex>
+          </GridItem>
+          <GridItem colStart={[1,1,2]} colEnd={[1,1,3]} justifySelf={"center"}>
+            <Image src={heroImage} alt="hero image" height={[300,378]} />
+          </GridItem>
+        </Grid>
+        {/* SECTION ONE END*/}
 
-      {/* SECTION TWO*/}
-      <Grid templateColumns={['1fr','1fr','1fr',"repeat(3, 1fr)"]} gap={10} pb={[50,50,50,100]}>
-        <GridItem>
-          <ActionCard
-            icon={MagicWand}
-            heading={"Mint"}
-            text={
-              "Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Integer posuere erat a ante venenatis dapibus posuere velit aliquet."
-            }
-          />
-        </GridItem>
-        <GridItem>
-          <ActionCard
-            icon={Exchange}
-            heading={"Trade"}
-            text={
-              "Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec sed odio dui."
-            }
-          />
-        </GridItem>
-        <GridItem>
-          <ActionCard
-            icon={Fire}
-            heading={"Burn"}
-            text={
-              "Sed posuere consectetur est at lobortis. Nulla vitae elit libero, a pharetra augue. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor."
-            }
-          />
-        </GridItem>
-      </Grid>
-      {/* SECTION TWO END*/}
+        {/* SECTION TWO*/}
+        <Grid templateColumns={['1fr','1fr','1fr',"repeat(3, 1fr)"]} gap={10} pb={[50,50,50,100]}>
+          <GridItem>
+            <ActionCard
+              icon={MagicWand}
+              heading={"Mint"}
+              text={
+                "Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Integer posuere erat a ante venenatis dapibus posuere velit aliquet."
+              }
+            />
+          </GridItem>
+          <GridItem>
+            <ActionCard
+              icon={Exchange}
+              heading={"Trade"}
+              text={
+                "Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec sed odio dui."
+              }
+            />
+          </GridItem>
+          <GridItem>
+            <ActionCard
+              icon={Fire}
+              heading={"Burn"}
+              text={
+                "Sed posuere consectetur est at lobortis. Nulla vitae elit libero, a pharetra augue. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor."
+              }
+            />
+          </GridItem>
+        </Grid>
+        {/* SECTION TWO END*/}
 
-      {/* SECTION THREE*/}
-      <Grid templateColumns={['1fr','1fr','1fr',"35% 1fr"]} gap={[10,10,10,null]} pb={[10,10,10,20]}>
-        <GridItem>
-          <Heading color={"white"}>Launching in</Heading>
-          <Image src={curlyUnderline} />
-        </GridItem>
-        <GridItem>
-          <Flex justifyContent={"space-between"}>
-            <DataDisplay number={10} state="Days" />
-            <DataDisplay number={20} state="Hours" />
-            <DataDisplay number={30} state="Minutes" />
-            <DataDisplay number={40} state="Seconds" />
-          </Flex>
-        </GridItem>
-      </Grid>
-      {/* SECTION THREE END*/}
+        {/* SECTION THREE*/}
+        <Grid templateColumns={['1fr','1fr','1fr',"35% 1fr"]} gap={[10,10,10,null]} pb={[10,10,10,20]}>
+          <GridItem>
+            <Heading color={"white"}>Launching in</Heading>
+            <Image src={curlyUnderline} />
+          </GridItem>
+          <GridItem>
+            <Flex justifyContent={"space-between"}>
+              <DataDisplay number={10} state="Days" />
+              <DataDisplay number={20} state="Hours" />
+              <DataDisplay number={30} state="Minutes" />
+              <DataDisplay number={40} state="Seconds" />
+            </Flex>
+          </GridItem>
+        </Grid>
+        {/* SECTION THREE END*/}
 
-      {/* SECTION FOUR*/}
-      <Grid gap={10} bg={"blackAlpha.100"} p={[5,5,5,10]} borderRadius={20} mb={[10,10,10,20]}>
-        <GridItem >
-          <Heading color={"white"}>Goats mechanics</Heading>
-          <Text color={"textColor2"}>
-            Each base goat will consist of one to six attributes that can be
-            outfitted with different traits to build a complete goat. These
-            traits can be traded, equipped to your other goats, burned for
-            tokens to roll for new traits, or utilized in other projects built
-            on top of GoatedGoats traits.
-          </Text>
-        </GridItem>
-        <GridItem>
-          <Grid templateColumns={["repeat(2, 1fr)","repeat(4, 1fr)"]} gap={[5,5,5,8]}>
-            <DataDisplay number="10,000" state="Total goats" sizeSmall start />
-            <DataDisplay number="16" state="Backgrounds" sizeSmall start />
-            <DataDisplay number="20" state="Skins" sizeSmall start />
-            <DataDisplay number="45" state="Clothes" sizeSmall start />
-            <DataDisplay number="30" state="Hats" sizeSmall start />
-            <DataDisplay number="15" state="Mouths" sizeSmall start />
-            <DataDisplay number="10" state="Eyes" sizeSmall start />
-            <DataDisplay number="5" state="Super Goats" sizeSmall start />
-          </Grid>
-        </GridItem>
-      </Grid>
-      {/* SECTION FOUR END*/}
-      <hr />
-      {/* SECTION FIVE*/}
-      <Box py={[10,10,10,20]} textAlign={"center"}>
-        <Flex flexDirection={"column"} alignItems={"center"}>
-          <Heading ding color={"white"}>
-            Get your GOATs
-          </Heading>
-          <Image
-            src={normalUnderline}
-            alt="normal underline"
-            height={15}
-            width={115}
-            marginLeft={40}
-          />
-        </Flex>
-        <Box
-          width={"min-content"}
-          color={"white"}
-          whiteSpace={"nowrap"}
-          bg={"whiteAlpha.50"}
-          py={1}
-          px={3}
-          mx={"auto"}
-          my={10}
-        >
-          <Text>
-            Sold out: <span>0/10,000</span>
-          </Text>
-        </Box>
-        <Flex justifyContent={"space-between"} direction={['column','column','column','row']} gap={10}>
-          <PackageCard
-            image={PackageImage1}
-            packageName={"Single GOAT"}
-            quantity={"1 GOAT = 3 FLOW"}
-            popular
-          />
-          <PackageCard
-            image={PackageImage2}
-            packageName={"Tripple GOAT"}
-            quantity={"3 GOAT = 7 FLOW"}
-            popular
-          />
-          <PackageCard
-            image={PackageImage3}
-            packageName={"GOAT Squad"}
-            quantity={"10 GOAT = 20 FLOW"}
-            popular
-          />
-        </Flex>
-        <Button color={"white"} bg={"primary"} mt={10}>
-          Praticipate in minting <Image src={shoppingCard} ml={3} />
-        </Button>
-      </Box>
-      {/* SECTION FIVE END*/}
-
-      {/* SECTION SIX*/}
-      <Box mb={20}>
-        <Flex flexDirection={"column"} alignItems={"center"}>
-          <Heading ding color={"white"}>
-            Team
-          </Heading>
-          <Image src={zikzakUnderline} alt="normal underline" mb={10} />
-          <Box width={['100%','100%','100%',800]} textAlign={"center"} mb={10}>
+        {/* SECTION FOUR*/}
+        <Grid gap={10} bg={"blackAlpha.100"} p={[5,5,5,10]} borderRadius={20} mb={[10,10,10,20]}>
+          <GridItem >
+            <Heading color={"white"}>Goats mechanics</Heading>
             <Text color={"textColor2"}>
-              Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum
-              nibh, ut fermentum massa justo sit amet risus. Vivamus sagittis
-              lacus vel augue laoreet rutrum faucibus dolor auctor.
+              Each base goat will consist of one to six attributes that can be
+              outfitted with different traits to build a complete goat. These
+              traits can be traded, equipped to your other goats, burned for
+              tokens to roll for new traits, or utilized in other projects built
+              on top of GoatedGoats traits.
+            </Text>
+          </GridItem>
+          <GridItem>
+            <Grid templateColumns={["repeat(2, 1fr)","repeat(4, 1fr)"]} gap={[5,5,5,8]}>
+              <DataDisplay number="10,000" state="Total goats" sizeSmall start />
+              <DataDisplay number="16" state="Backgrounds" sizeSmall start />
+              <DataDisplay number="20" state="Skins" sizeSmall start />
+              <DataDisplay number="45" state="Clothes" sizeSmall start />
+              <DataDisplay number="30" state="Hats" sizeSmall start />
+              <DataDisplay number="15" state="Mouths" sizeSmall start />
+              <DataDisplay number="10" state="Eyes" sizeSmall start />
+              <DataDisplay number="5" state="Super Goats" sizeSmall start />
+            </Grid>
+          </GridItem>
+        </Grid>
+        {/* SECTION FOUR END*/}
+        <hr />
+        {/* SECTION FIVE*/}
+        <Box py={[10,10,10,20]} textAlign={"center"}>
+          <Flex flexDirection={"column"} alignItems={"center"}>
+            <Heading ding color={"white"}>
+              Get your GOATs
+            </Heading>
+            <Image
+              src={normalUnderline}
+              alt="normal underline"
+              height={15}
+              width={115}
+              marginLeft={40}
+            />
+          </Flex>
+          <Box
+            width={"min-content"}
+            color={"white"}
+            whiteSpace={"nowrap"}
+            bg={"whiteAlpha.50"}
+            py={1}
+            px={3}
+            mx={"auto"}
+            my={10}
+          >
+            <Text>
+              Sold out: <span>0/10,000</span>
             </Text>
           </Box>
-        </Flex>
-        <Grid templateColumns={['1fr','1fr',"repeat(2, 1fr)","repeat(4, 1fr)"]} gap={5} justifyContent={"center"}>
-          <TeamCard image={TeamImage1} text={"@loremipsum"} />
-          <TeamCard image={TeamImage2} text={"@loremipsum"} />
-          <TeamCard image={TeamImage3} text={"@loremipsum"} />
-          <TeamCard image={TeamImage4} text={"@loremipsum"} />
-        </Grid>
+          <Flex justifyContent={"space-between"} direction={['column','column','column','row']} gap={10}>
+            <PackageCard
+              image={PackageImage1}
+              packageName={"Single GOAT"}
+              quantity={"1 GOAT = 3 FLOW"}
+              popular
+            />
+            <PackageCard
+              image={PackageImage2}
+              packageName={"Tripple GOAT"}
+              quantity={"3 GOAT = 7 FLOW"}
+              popular
+            />
+            <PackageCard
+              image={PackageImage3}
+              packageName={"GOAT Squad"}
+              quantity={"10 GOAT = 20 FLOW"}
+              popular
+            />
+          </Flex>
+          <Button color={"white"} bg={"primary"} mt={10}>
+            Praticipate in minting <Image src={shoppingCard} ml={3} />
+          </Button>
+        </Box>
+        {/* SECTION FIVE END*/}
+
+        {/* SECTION SIX*/}
+        <Box mb={20}>
+          <Flex flexDirection={"column"} alignItems={"center"}>
+            <Heading ding color={"white"}>
+              Team
+            </Heading>
+            <Image src={zikzakUnderline} alt="normal underline" mb={10} />
+            <Box width={['100%','100%','100%',800]} textAlign={"center"} mb={10}>
+              <Text color={"textColor2"}>
+                Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum
+                nibh, ut fermentum massa justo sit amet risus. Vivamus sagittis
+                lacus vel augue laoreet rutrum faucibus dolor auctor.
+              </Text>
+            </Box>
+          </Flex>
+          <Grid templateColumns={['1fr','1fr',"repeat(2, 1fr)","repeat(4, 1fr)"]} gap={5} justifyContent={"center"}>
+            <TeamCard image={TeamImage1} text={"@loremipsum"} />
+            <TeamCard image={TeamImage2} text={"@loremipsum"} />
+            <TeamCard image={TeamImage3} text={"@loremipsum"} />
+            <TeamCard image={TeamImage4} text={"@loremipsum"} />
+          </Grid>
+        </Box>
+        {/* SECTION SIX END*/}
+        <hr />
+        {/* SECTION SEVEN*/}
+        <Box mt={20}>
+          <Flex justifyContent={"center"} mb={10}>
+            <Heading color={"white"}>FAQ</Heading>
+            <Image src={question} ml={2} />
+          </Flex>
+          <Flex width={['100%','100%','100%',800]} mx={"auto"} gap={4} flexDirection={"column"}>
+            <Flex
+              p={3}
+              color={"white"}
+              alignItems={"center"}
+              justifyContent={"space-between"}
+              bg={"whiteAlpha.100"}
+              cursor={"pointer"}
+            >
+              <Text>
+                Maecenas sed diam eget risus varius blandit sit amet non magna.
+              </Text>
+              <Image src={arrow} alt="down arrow icon" />
+            </Flex>
+            <Flex
+              p={3}
+              color={"white"}
+              alignItems={"center"}
+              justifyContent={"space-between"}
+              bg={"whiteAlpha.100"}
+              cursor={"pointer"}
+            >
+              <Text>
+                Maecenas sed diam eget risus varius blandit sit amet non magna.
+              </Text>
+              <Image src={arrow} alt="down arrow icon" />
+            </Flex>
+            <Flex
+              p={3}
+              color={"white"}
+              alignItems={"center"}
+              justifyContent={"space-between"}
+              bg={"whiteAlpha.100"}
+              cursor={"pointer"}
+            >
+              <Text>
+                Maecenas sed diam eget risus varius blandit sit amet non magna.
+              </Text>
+              <Image src={arrow} alt="down arrow icon" />
+            </Flex>
+            <Flex
+              p={3}
+              color={"white"}
+              alignItems={"center"}
+              justifyContent={"space-between"}
+              bg={"whiteAlpha.100"}
+              cursor={"pointer"}
+            >
+              <Text>
+                Maecenas sed diam eget risus varius blandit sit amet non magna.
+              </Text>
+              <Image src={arrow} alt="down arrow icon" />
+            </Flex>
+            <Flex
+              p={3}
+              color={"white"}
+              alignItems={"center"}
+              justifyContent={"space-between"}
+              bg={"whiteAlpha.100"}
+              cursor={"pointer"}
+            >
+              <Text>
+                Maecenas sed diam eget risus varius blandit sit amet non magna.
+              </Text>
+              <Image src={arrow} alt="down arrow icon" />
+            </Flex>
+          </Flex>
+        </Box>
+        {/* SECTION SEVEN END*/}
       </Box>
-      {/* SECTION SIX END*/}
-      <hr />
-      {/* SECTION SEVEN*/}
-      <Box mt={20}>
-        <Flex justifyContent={"center"} mb={10}>
-          <Heading color={"white"}>FAQ</Heading>
-          <Image src={question} ml={2} />
-        </Flex>
-        <Flex width={['100%','100%','100%',800]} mx={"auto"} gap={4} flexDirection={"column"}>
-          <Flex
-            p={3}
-            color={"white"}
-            alignItems={"center"}
-            justifyContent={"space-between"}
-            bg={"whiteAlpha.100"}
-            cursor={"pointer"}
-          >
-            <Text>
-              Maecenas sed diam eget risus varius blandit sit amet non magna.
-            </Text>
-            <Image src={arrow} alt="down arrow icon" />
-          </Flex>
-          <Flex
-            p={3}
-            color={"white"}
-            alignItems={"center"}
-            justifyContent={"space-between"}
-            bg={"whiteAlpha.100"}
-            cursor={"pointer"}
-          >
-            <Text>
-              Maecenas sed diam eget risus varius blandit sit amet non magna.
-            </Text>
-            <Image src={arrow} alt="down arrow icon" />
-          </Flex>
-          <Flex
-            p={3}
-            color={"white"}
-            alignItems={"center"}
-            justifyContent={"space-between"}
-            bg={"whiteAlpha.100"}
-            cursor={"pointer"}
-          >
-            <Text>
-              Maecenas sed diam eget risus varius blandit sit amet non magna.
-            </Text>
-            <Image src={arrow} alt="down arrow icon" />
-          </Flex>
-          <Flex
-            p={3}
-            color={"white"}
-            alignItems={"center"}
-            justifyContent={"space-between"}
-            bg={"whiteAlpha.100"}
-            cursor={"pointer"}
-          >
-            <Text>
-              Maecenas sed diam eget risus varius blandit sit amet non magna.
-            </Text>
-            <Image src={arrow} alt="down arrow icon" />
-          </Flex>
-          <Flex
-            p={3}
-            color={"white"}
-            alignItems={"center"}
-            justifyContent={"space-between"}
-            bg={"whiteAlpha.100"}
-            cursor={"pointer"}
-          >
-            <Text>
-              Maecenas sed diam eget risus varius blandit sit amet non magna.
-            </Text>
-            <Image src={arrow} alt="down arrow icon" />
-          </Flex>
-        </Flex>
-      </Box>
-      {/* SECTION SEVEN END*/}
-    </Box>
+    </>
   );
 }
